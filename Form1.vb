955332f09f7a4d9935912a4c6eb47a4cc49089a3
James <44694042+Kayseptic@users.noreply.github.com>
2019-09-16 11:07:41 -0700

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Dim rS As String
+    Dim r As New Random
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim strBuff As New String(T1.Text.ToCharArray.OrderBy(Function(c) r.NextDouble).ToArray)
+        Dim AlphaNumericString As String = Guid.NewGuid.ToString
+        AlphaNumericString = AlphaNumericString.Replace("-", "")
+        rS = AlphaNumericString.ToUpper + AlphaNumericString
+        T1.Text = rS
+        T1.Text = New String(T1.Text.ToCharArray.OrderBy(Function(c) r.NextDouble).ToArray)
+
+    End Sub
+End Class
